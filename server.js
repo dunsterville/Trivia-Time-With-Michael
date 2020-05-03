00071edda8f95167d5fcb3efe56d09ca1b842faf
Michael Dunsterville <michael@klientboost.com>
2020-05-03 15:48:47 -0700

    Added dollar amount

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { join } = require('path')
 //const db = require('./config')
 const app = express()
 
-app.use(express.static(join(__dirname, 'public')))
+app.use(express.static(join(__dirname, 'public'), {extensions: ['html']}))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
