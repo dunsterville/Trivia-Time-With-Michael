e2ad271ef8e501cbd01986b5f4755aa4c3c98359
Michael Dunsterville <michael@klientboost.com>
2020-05-03 14:10:29 -0700

    added a date to score

--- models/Score.js
+++ models/Score.js
@@ -2,6 +2,7 @@ module.exports = (model, Schema) => {
 
   const Score = new Schema({
     score: { type: Number, required: true },
+    date: { type: Date, default: Date.now},
     user: { type: Schema.Types.ObjectId, ref: 'User'},
   }, {timestamps: {createdAt: 'createdAt', updatedAt: 'updatedAt'}})
 
