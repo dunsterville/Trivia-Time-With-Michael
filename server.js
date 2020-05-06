26a9cf235b7e5efb4579095bc38ce2cc9602281c
Michael Dunsterville <michael@klientboost.com>
2020-05-06 13:29:43 -0700

    SSL base setup

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { join } = require('path')
 //const db = require('./config')
 const app = express()
 
-app.use(express.static(join(__dirname, 'public'), {extensions: ['html']}))
+app.use(express.static(join(__dirname, 'public'), {extensions: ['html'], dotfiles: 'allow' }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
