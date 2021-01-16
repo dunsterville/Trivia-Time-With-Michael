7c737012015dc312e6eaa2bb9ae1a15cd5402d9e
Michael Dunsterville <michael@klientboost.com>
2021-01-16 14:47:23 -0800

    Got basses of Signature working

--- client/src/components/SignUpForm/SignUpForm.js
+++ client/src/components/SignUpForm/SignUpForm.js
@@ -1,26 +1,36 @@
-import React, { useContext }  from "react"
-import { useForm } from "react-hook-form"
+import React, { useContext, useState }  from 'react'
+import { useForm } from 'react-hook-form'
+import SignatureCanvas from 'react-signature-canvas'
 import UserContext from '../../utils/Usercontext'
 
 const SignUpForm = _ => {
-  const { register, handleSubmit, watch, errors } = useForm()
-  //const { username,  password, handleOnSubmit } = useContext(UserContext)
-  const onSubmit = data => console.log(data)
+  const { register, handleSubmit, errors } = useForm()
 
-  console.log(watch("example")); // watch input value by passing the name of it
+
+  const [ userState, userSetState ] = useState({
+    trimmedDataURL: null
+  })
+  
+
+  //const { username,  password, handleOnSubmit } = useContext(UserContext)
+  const onSubmit = data => console.log(data, userState.sigCanvas.getTrimmedCanvas().toDataURL('image/svg+xml'))
 
   return (
     <div>
       {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
       <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
-        <input name="username" ref={register({ required: true })} />
-        {errors.username && <span>This field is required</span>}
+        <input name="username" ref={register({ required: true, minLength: 4 })} />
+        {errors.username && TypeError==="required" && <span>This field is required</span>}
+        {errors.username && TypeError==="minLength" && <span>Username must be at least 4 characters</span>}
         
+        <SignatureCanvas canvasProps={{width: 500, height: 200, className: 'sigCanvas'}} ref={(ref) => { userState.sigCanvas = ref }} />
+
         {/* include validation with required or other standard HTML validation rules */}
-        <input name="password" ref={register({ required: true })} />
+        <input name="password" ref={register({ required: true , minLength: 8})} />
         {/* errors will return when field validation fails  */}
-        {errors.password && <span>This field is required</span>}
+        {errors.password && TypeError==="required" && <span>This field is required</span>}
+        {errors.password && TypeError==="minLength" && <span>Password must be at least 8 characters</span>}
         
         <input type="submit" />
       </form>
