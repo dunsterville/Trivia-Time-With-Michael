b41a64ee92f0d269f5c53c67b20defb2702f4590
Michael Dunsterville <michael@klientboost.com>
2021-06-01 10:51:53 -0700

    changed to wss

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import io from 'socket.io-client'
 
-const socket = io.connect('https://dev.triviatimewithmichael.com/')
+const socket = io.connect('wss://dev.triviatimewithmichael.com/')
 
 const Admin = _ => {
 
